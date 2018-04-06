6784ca35f8d8b3617892d073305b732f4fca4d30
Matheus Macedo <matheuscont@hotmail.com>
2018-04-06 11:54:27 -0300

    Curso Eduardo Pires

---
+++ src/EP.CursoMVC.Domain/Entity/Cliente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EP.CursoMVC.Domain.Entity
+{
+    public class Cliente
+    {
+        public Cliente()
+        {
+            ClienteId = Guid.NewGuid();
+        }
+
+        public Guid ClienteId { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string Email { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public bool Ativo { get; set; }
+        public virtual ICollection<Endereco> Enderecos { get; set; } //virtual = pode ser sobrescrita e habilita o layzlong
+        
+    }
+}
---
+++ src/EP.CursoMVC.UI.Site/EP.CursoMVC.UI.Site/Startup.cs
@@ -0,0 +1,15 @@
+﻿using EP.CursoMVC.UI.Site;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace EP.CursoMVC.UI.Site
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
